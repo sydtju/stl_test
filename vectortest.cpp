9a2630ef34b13a0286ae6c8c34d2cd2588815ab7
yadongshao <348829719@qq.com>
2015-06-03 23:59:54 +0800

    Update vectortest.cpp

--- vectortest.cpp
+++ vectortest.cpp
@@ -19,6 +19,7 @@ void testvector(){
 	cout<<"\nvector的最后一个元素是："<<avector.back()<<endl;
 	cout<<"\nvector删除最后一个元素后：";
 	avector.erase(avector.begin()+3);//erase是删除元素后 指向下一个元素的位置  所以当删除最后一个元素的时候会出错
+	//还可以删除一个范围内的数据
 	for(pointer=avector.begin();pointer!=avector.end();pointer++){
 		cout<<*pointer<<"   ";
 	}
